fa772ee543fc86aea4259dc4449b4a9c97993724
burn-Thor <eothorburn@gmail.com>
2022-07-04 11:07:40 +0100

    port working

--- src/server.js
+++ src/server.js
@@ -1,5 +1,11 @@
 const express = require("express"); //import entrie express library
 const app = express(); //creating our app object, that houses our server instructions
+const port = process.env.PORT || 5001;
 
 app.use("/static", express.static("public")); 
 //telling app to use the public folder, when a request is made to /static 
+
+app.listen(port, () => {
+    console.log(`listening on post ${port}`);
+});
+//listening on the given port for requests to our webserver 
