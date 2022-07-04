db6ddc984fb1bf5e0a90ccc2f92a368550f3b1f6
burn-Thor <eothorburn@gmail.com>
2022-07-04 10:46:58 +0100

    git_ignore

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require("express"); //import entrie express library
 const app = express(); //creating our app object, that houses our server instructions
 
-app.use()
+app.use("/static", express.static("public")); 
+//telling app to use the public folder, when a request is made to /static 
